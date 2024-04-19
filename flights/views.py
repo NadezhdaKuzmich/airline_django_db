247e5a1696d2c9352ca4c647283d59efbb5a58f6
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-19 23:34:23 +0200

    add booking

--- flights/views.py
+++ flights/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import Flight
+from django.urls import reverse
+
+from .models import Flight, Passenger
 
 
 def index(request):
@@ -13,5 +16,15 @@ def flight(request, flight_id):
 
     return render(request, "flights/flight.html", {
         "flight": current_flight,
-        "passengers": current_flight.passengers.all()
+        "passengers": current_flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=current_flight).all()
     })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        current_flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(current_flight)
+        return HttpResponseRedirect(
+            reverse("flight", args=(current_flight.id,)))
