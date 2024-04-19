acbe9e3c153f2f3d6601a855614b3b0d381c1c47
NadezhdaKuzmich <speranza.kuzmich@gmail.com>
2024-04-19 22:08:21 +0200

    add flight page

--- flights/views.py
+++ flights/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from .models import Flight
 
+
 def index(request):
     return render(request, "flights/index.html", {
         "flights": Flight.objects.all()
+    })
+
+
+def flight(request, flight_id):
+    current_flight = Flight.objects.get(pk=flight_id)
+
+    return render(request, "flights/flight.html", {
+        "flight": current_flight
     })
